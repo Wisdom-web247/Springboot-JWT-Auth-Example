a9f5be459b47a64bf1403edecc2abc6ef31630a1
Wisdom Mahlangu <wizzymahlangu@gmail.com>
2023-09-13 16:20:33 +0200

    deleted userDetails interface and Service file

--- src/main/java/Services/UserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package Services;
-
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserDetailsService {
-        UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-    }
-
